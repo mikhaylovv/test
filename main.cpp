2c1c9e82af0514205c8f63aa455bb7b7adcc3b86
thesanone <thesanone@bk.ru>
2019-02-22 02:26:43 +0300

    multiplexor class name changed

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 
 
 template<class T>
-class add_res { 
+class multiplexor { 
 public:
-	add_res(int val) : _val(val) {}
+	multiplexor(int val) : _val(val) {}
 	
-	add_res<T>& operator()(int val) {
+	multiplexor<T>& operator()(int val) {
 		_val = T{}(_val, val);
 		return *this; 
 	} 
@@ -21,8 +21,8 @@ private:
 	int _val = 0;
 }; 
 
-add_res<std::plus<>> add(int val) { 
-	return add_res<std::plus<>>(val); 
+multiplexor<std::plus<>> add(int val) { 
+	return multiplexor<std::plus<>>(val); 
 } 
 
 int main() { 
